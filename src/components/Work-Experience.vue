c677eae6a7edd0256c4261046b7794911065933e
root <johnraydeluna@gmail.com>
2026-04-23 18:03:18 +0800

    w

--- src/components/Work-Experience.vue
+++ src/components/Work-Experience.vue
@@ -196,7 +196,7 @@
                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                       <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                     </svg>
-                    March 2018 – February 2025
+                    March 2018 – December 2024
                   </span>
                   <span class="bg-purple-100 dark:bg-purple-900 text-purple-800 dark:text-purple-200 px-3 py-1 rounded-full text-xs font-medium">
                     7 Years Experience
